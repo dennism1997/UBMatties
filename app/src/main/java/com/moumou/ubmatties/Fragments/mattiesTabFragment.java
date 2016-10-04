6bffa735ad98060537580c4dc2afc40990b65dcd
dennism1997 <D.Mouwen@student.tudelft.nl>
2016-10-04 19:02:06 +0200

    improved a bit, dont know how to get and test with friend list

--- app/src/main/java/com/moumou/ubmatties/Fragments/mattiesTabFragment.java
+++ app/src/main/java/com/moumou/ubmatties/Fragments/mattiesTabFragment.java
@@ -9,15 +9,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.moumou.ubmatties.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * Created by MouMou on 04-10-16
@@ -30,6 +41,8 @@ public class MattiesTabFragment extends Fragment {
     private LoginResult loginResult;
     private TextView textView;
     private ProfileTracker profileTracker;
+    private Collection<String> permissionList;
+    private LinkedList<String> friendsList;
 
     public MattiesTabFragment() {
         super();
@@ -44,24 +57,28 @@ public class MattiesTabFragment extends Fragment {
         textView.setText("second tab");
 
         loginButton = (LoginButton) view.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
+
+        //loginButton.setReadPermissions(permissionList);
+
         // If using in a fragment
         loginButton.setFragment(this);
         // Other app specific specialization
+        LoginManager.getInstance().logInWithReadPermissions(MattiesTabFragment.this, permissionList);
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                System.out.println("ja");
                 textView.setText(
+
                         "User ID: "
                                 + loginResult.getAccessToken().getUserId()
                                 + "\n" +
                                 "Auth Token: "
                                 + loginResult.getAccessToken().getToken()
                 );
-                loginButton.setVisibility(View.GONE);
+
+                //loginButton.setVisibility(View.GONE);
             }
 
             @Override
@@ -72,6 +89,7 @@ public class MattiesTabFragment extends Fragment {
             @Override
             public void onError(FacebookException exception) {
                 textView.setText("Login attempt failed.");
+                System.out.println(exception.toString());
             }
         });
 
@@ -86,20 +104,68 @@ public class MattiesTabFragment extends Fragment {
         callbackManager = CallbackManager.Factory.create();
 
 
+        permissionList = new LinkedList<String>();
+        permissionList.add("email");
+        permissionList.add("user_friends");
+
+        friendsList = new LinkedList();
+
         profileTracker = new ProfileTracker() {
             @Override
             protected void onCurrentProfileChanged(
                     Profile oldProfile,
                     Profile currentProfile) {
-                textView.setText(currentProfile.getName());
-                loginButton.setVisibility(View.GONE);
+                if (currentProfile != null) {
+                    textView.setText(currentProfile.getName());
+                    //loginButton.setVisibility(View.GONE);
+                    readFriendsList(currentProfile);
+                } else {
+                    loginButton.setVisibility(View.VISIBLE);
+                }
             }
         };
+
 
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void fillFriendsList(JSONArray jsonArray) throws JSONException {
+        if (friendsList == null) {
+            friendsList = new LinkedList();
+        }
+        System.out.println("size = " + jsonArray.length());
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            friendsList.add(jsonArray.get(i).toString());
+            System.out.println(jsonArray.get(i).toString());
+        }
+
+        //textView.setText(friendsList.get(0));
+    }
+
+    public void readFriendsList(final Profile currentProfile) {
+        /* make the API call */
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/" + currentProfile.getId() + "",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                            /* handle the result */
+                        try {
+                            System.out.println(response.getJSONObject().toString());
+                            JSONArray jsonArrayFriends = response.getJSONObject().getJSONArray("data");
+                            fillFriendsList(jsonArrayFriends);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
     }
 }
