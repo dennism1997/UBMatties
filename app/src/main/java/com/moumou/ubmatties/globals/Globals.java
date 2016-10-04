56539aaab4152ff3b763222f5b68332687f51f48
dennism1997 <D.Mouwen@student.tudelft.nl>
2016-10-04 14:57:33 +0200

    first commit

---
+++ app/src/main/java/com/moumou/ubmatties/globals/Globals.java
@@ -0,0 +1,13 @@
+package com.moumou.ubmatties.globals;
+
+/**
+ * Created by dennis on 04-10-16.
+ */
+
+public class Globals {
+    public static final int NUMBER_OF_TABS = 2;
+
+    private Globals(){
+
+    }
+}
